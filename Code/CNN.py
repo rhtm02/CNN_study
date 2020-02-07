bc443057333d0e7a3e55552ddb5901673da31908
Lee hyungjun <rhtm02@gmail.com>
2020-02-07 15:13:14 +0900

    Code init

---
+++ Code/CNN.py
@@ -0,0 +1,20 @@
+# this file is CNN network configuration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if ( __name__ == "__main__"):
+    print("CNN module")
+
+else:
+    print("import CNN module")
---
+++ Code/main.py
@@ -0,0 +1,16 @@
+# this file is main file
+
+
+
+
+
+if (__name__ == "__main__"):
+
+    try:
+        pass
+    except:
+        pass
+
+
+else:
+    pass
