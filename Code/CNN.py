2e85f2372345e775b84a1330621dad527722304d
Lee hyungjun <rhtm02@gmail.com>
2020-02-16 20:08:35 +0900

    CNN with BatchNorm pooling Layer

--- Code/CNN.py
+++ Code/CNN.py
@@ -1,40 +1,66 @@
 # this file is CNN network configuration
+import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+use_cuda = torch.cuda.is_available()
+#batch norm
 class Network(nn.Module):
 
-    def __init__(self):
+    def __init__(self, kernalsize = (4,4)):
         super(Network, self).__init__()
-        #inirialize convelution layer
-        self.conv1 = nn.Conv2d()
-        self.conv2 = nn.Conv2d()
-        self.conv3 = nn.Conv2d()
-        self.conv4 = nn.Conv2d()
+        #convelution layer
+        self.conv_module = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=2, stride=1, kernel_size=kernalsize),
+            #print("C1"),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features = 2),
+            nn.Conv2d(in_channels=2, out_channels=4, stride=1, kernel_size=kernalsize, padding=(3, 3)),
+            #print("C2"),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features = 4),
+            nn.Conv2d(in_channels=4, out_channels=8, stride=1, kernel_size=kernalsize, padding=(3, 3)),
+            #print("C3"),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features = 8),
+            nn.Conv2d(in_channels=8, out_channels=16, stride=1, kernel_size=kernalsize),
+            #print("C4"),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features = 16)
+        )
+        # Affine layer
+        self.affine_module = nn.Sequential(
+            nn.Linear(in_features = 16*28*28, out_features = 1200),
+            nn.ReLU(),
+            nn.Linear(in_features=1200, out_features=100),
+            nn.ReLU(),
+            nn.Linear(in_features=100, out_features=10)
+        )
 
-        #initialize pooling layer
-        self.pooling = nn.MaxPool2d()
+
+        if (use_cuda):
+            self.conv_module = self.conv_module.cuda()
+            self.affine_module  =self.affine_module.cuda()
+            print("USE CUDA")
 
     def forward(self, x):
 
+        out = self.conv_module(x)
+        #print("Conv output : " + str(out.shape))
+        #flatten
+        dim = 1
+        for d in out.size()[1:]:
+            dim = dim * d
+        out = out.view(-1, dim)
 
+        out = self.affine_module(out)
 
-        return x
-
-
-
-
-
-
-
-
-
-
-
-
+        return F.softmax(out, dim = 1)
 
 
 
 if ( __name__ == "__main__"):
     print("CNN module")
+
 else:
-    print("import CNN module")
+    pass
+    #print("import CNN module")
