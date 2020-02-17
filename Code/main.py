84bb53799c9bed24aa6f4e253f790b871455514b
Lee hyungjun <rhtm02@gmail.com>
2020-02-17 11:52:54 +0900

    add import CNN scripts

--- Code/main.py
+++ Code/main.py
@@ -1,32 +1,37 @@
 # this file is main file
-from CNN import *
-import CNN
+
+#from CNN_2layer import Network, use_cuda
+from CNN import Network, use_cuda
+#from CNN_maxpooling import Network, use_cuda
 import DataLoader
 import torch
 
+torch.cuda.empty_cache()
 # set hyperparameters
 EPOCH = 10
 LEARNING_RATE = 1e-3
 KERNALSIZE = (5,5)
+BATCHSIZE = 1
 
 if (__name__ == "__main__"):
     #model definition
     model = Network()
+    print(model)
     #dataloader definotion
-    trainloader, testlodaer = DataLoader.LoadData()
+    trainloader, testlodaer = DataLoader.LoadData(BatchSize = BATCHSIZE)
     #configure loss function
     criterion = torch.nn.CrossEntropyLoss().to("cuda")
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     #training
     for epoch in range(EPOCH):
-        avg_cost = 0
+
 
         for i, data in enumerate(trainloader):
             # image is already size of (28x28), no reshape
             # label is not one-hot encoded
             x, y = data
-            if (CNN.use_cuda):
+            if (use_cuda):
                 x = x.to("cuda")
                 y = y.to("cuda")
 
@@ -36,10 +41,25 @@ if (__name__ == "__main__"):
             cost = criterion(hypothesis, y)
             cost.backward()
             optimizer.step()
-            avg_cost += cost / len(trainloader)
-
-        print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
 
 
+        print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, cost))
+
+    # save trained model
+
+    #torch.save(model.state_dict())
+
+
+    # test session
+    with torch.no_grad():
+        for i, data in enumerate(trainloader):
+            X_test, Y_test = data
+            if(use_cuda):
+                X_test = X_test.to("cuda")
+                Y_test = Y_test.to("cuda")
+            prediction = model(X_test)
+            correct_prediction = torch.argmax(prediction, 1) == Y_test
+            accuracy = correct_prediction.float().mean()
+            print('Accuracy:', accuracy.item())
 else:
     pass
